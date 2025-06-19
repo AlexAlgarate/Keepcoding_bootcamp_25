04f72612d206e5ea2027079e36c7eec01e7ce0c4
Alex Algarate <alex.algarate@gmail.com>
2025-06-19 08:32:59 +0200

    Add some testing to 18/06

--- python_course/P101/classes/18_06/18_06.py
+++ python_course/P101/classes/18_06/18_06.py
@@ -1,3 +1,5 @@
+import pytest
+
 MOBILE_KEYS = {
     "1": ".,?!:",
     "2": "ABC",
@@ -12,21 +14,39 @@ MOBILE_KEYS = {
 }
 
 
-def create_dictionary() -> dict:
-    return {
+def create_dictionary() -> dict: ...
+
+
+def message_to_keys(message: str) -> str:
+    map_dict = {
         char: key * (i + 1)
         for key, chars in MOBILE_KEYS.items()
         for i, char in enumerate(chars)
     }
+    print(map_dict, "\n")
+    return "".join([map_dict[char] for char in message.upper() if char in map_dict])
 
 
-def message_to_keys(message: str) -> str:
-    map = create_dictionary()
-    return "".join([map[char] for char in message.upper() if char in map])
+solution = "0"
 
-
-solution = "4433555555666110966677755531111"
-
-sol_1 = message_to_keys("Hello, World!")
+sol_1 = message_to_keys(" ")
 print(sol_1)
 print(solution == sol_1)
+
+
+class TestMobile:
+    @pytest.mark.parametrize(
+        "input_text, expected_output",
+        [
+            ("Hello, World!", "4433555555666110966677755531111"),
+            ("Python 3.10", "79998446666601"),
+            ("KeepCoding", "55333372226663444664"),
+            ("A B C", "20220222"),
+            (" ", "0"),
+        ],
+    )
+    def test_message_to_keys(self, input_text, expected_output):
+        assert message_to_keys(input_text) == expected_output
+
+    def test_message_to_keys_empty(self):
+        assert message_to_keys("") == ""
