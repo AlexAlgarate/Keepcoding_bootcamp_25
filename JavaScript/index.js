00691835d4cf0827a8682cfc0f8787b5b01a7910
Alex Algarate <alex.algarate@gmail.com>
2025-07-01 16:32:15 +0200

    Creating the JavaScript module

---
+++ JavaScript/index.js
@@ -0,0 +1,8 @@
+miPrimerString = 'Hola, mundo!';
+miSegundoString = '¡Adiós, mundo!';
+
+function concatenarCadenas(cadena1, cadena2) {
+  return cadena1 + ' ' + cadena2;
+}
+
+console.log(concatenarCadenas(miPrimerString, miSegundoString));
