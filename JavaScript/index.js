e32c9f1fead05965a7fecd8225a384bae8a690e1
Alex Algarate <alex.algarate@gmail.com>
2025-07-08 10:13:43 +0200

    First commit JavaScript

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,8 +1,42 @@
-miPrimerString = 'Hola, mundo!';
-miSegundoString = '¡Adiós, mundo!';
+// Variables
 
-function concatenarCadenas(cadena1, cadena2) {
-  return cadena1 + ' ' + cadena2;
+let cadena1 = 'Hola';
+
+let cadena2 = 'Mundo';
+let resultado = cadena1 + ' ' + cadena2;
+console.log(cadena1);
+console.log(cadena2);
+console.log(cadena1 + cadena2);
+console.log(resultado);
+
+console.log(`${cadena1} ${cadena2}`);
+
+console.log('Operadores lógicos', `${'a' === 'b'} returns False`);
+console.log('Operadores lógicos', `${'a' !== 'b'} returns True`);
+console.log('Operadores lógicos AND', `${true && false} returns True`);
+console.log('Operadores lógicos OR', `${'a' || 'b'} returns True`);
+
+// Funciones
+
+function helloFunction() {
+  return 'hola soy una funcion';
 }
 
-console.log(concatenarCadenas(miPrimerString, miSegundoString));
+function helloFunctionWithArguments(cadena1, cadena2) {
+  return `hola soy una funcion con dos argumentos: ${cadena1} y ${cadena2}`;
+}
+
+// Es anonima porque no lleva nombre después de la declaración de la funcion
+let anonymousFunction = function () {
+  return 'Hola funcion anonima';
+};
+
+// Similar a *arg en Python, los rest parameter...
+function restParamsFunction(...names) {
+  console.log(`Los invitados a la fiesta son: ${names} `);
+}
+
+console.log(helloFunction());
+console.log(anonymousFunction());
+console.log(helloFunctionWithArguments('primer argumento', 'segundo argumento'));
+console.log(restParamsFunction('Vicky', 'Sombra', 'Shuri', 'Luna'));
