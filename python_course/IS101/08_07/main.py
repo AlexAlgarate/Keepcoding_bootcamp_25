921a7beba6627da780602cf70fa480a505b9a39f
Alex Algarate <alex.algarate@gmail.com>
2025-07-15 10:26:31 +0200

    Drop old functions that never be used again

--- python_course/IS101/08_07/main.py
+++ python_course/IS101/08_07/main.py
@@ -28,51 +28,6 @@ ROMAN_TO_ARABIC_MAP: dict[str, int] = {
     "M": 1000,
 }
 
-ROMAN_TO_INT = {symbol: value for value, symbol in ARABIC_TO_ROMAN_MAP.items()}
-
-
-def is_valid_roman(roman: str) -> bool:
-    roman_pattern = re.compile(
-        "^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"
-    )
-    return bool(roman_pattern.fullmatch(roman.upper()))
-
-
-def from_arabic_to_roman(number: int) -> str:
-    if not 1 <= number < 4000:
-        raise ValueError("El número tiene que estar entre 0 y 3999.")
-
-    result = []
-
-    for value, numeral in ARABIC_TO_ROMAN_MAP.items():
-        count, number = divmod(number, value)
-        result.append(numeral * count)
-
-    return "".join(result)
-
-
-def from_roman_to_arabic(roman: str) -> int:
-    if not is_valid_roman(roman):
-        raise ValueError(f"Número romano no válido: '{roman}'")
-
-    roman = roman.upper()
-    result = 0
-    previous = 0
-
-    for char in reversed(roman):
-        value = ROMAN_TO_INT[char]
-
-        if char not in ROMAN_TO_INT:
-            raise ValueError(f"Símbolo romano no permitido: {char}")
-
-        if value < previous:
-            result -= value
-        else:
-            result += value
-            previous = value
-
-    return result
-
 
 class IRomanCalculator(ABC):
     @abstractmethod
