4fe122973bf9c1d71f9fe54a948753cca9de7560
Alex Algarate <alex.algarate@gmail.com>
2025-07-21 11:08:12 +0200

    Move functions into a class

--- python_course/IS101/morse_tkinter/app/models.py
+++ python_course/IS101/morse_tkinter/app/models.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 char_to_morse_dict = {
     "A": ".-",
     "B": "-...",
@@ -42,34 +44,41 @@ char_to_morse_dict = {
 morse_to_char_dict = {k: v for v, k in char_to_morse_dict.items()}
 
 
-def text_to_morse(text: str):
+class IMorseConverter(ABC):
+    @abstractmethod
+    def text_to_morse(self, text: str) -> str:
+        pass
+
+    @abstractmethod
+    def morse_to_text(self, morse: str) -> str:
+        pass
+
+
+class MorseConverter:
     WHITE_SPACE_WORDS = "   "
     WHITE_SPACE_LETTERS = " "
 
-    return WHITE_SPACE_WORDS.join(
-        WHITE_SPACE_LETTERS.join(
-            char_to_morse_dict[letter]
-            for letter in word
-            if letter in char_to_morse_dict
+    def text_to_morse(self, text: str) -> str:
+        return self.WHITE_SPACE_WORDS.join(
+            self.WHITE_SPACE_LETTERS.join(
+                char_to_morse_dict[letter]
+                for letter in word
+                if letter in char_to_morse_dict
+            )
+            for word in text.upper().split()
         )
-        for word in text.upper().split()
-    )
 
+    def morse_to_text(self, morse: str) -> str:
+        words = morse.strip().split(self.WHITE_SPACE_WORDS)
 
-def morse_to_text(morse: str):
-    WHITE_SPACE_WORDS = "   "
-    WHITE_SPACE_LETTERS = " "
+        decoded_words = []
 
-    words = morse.strip().split(WHITE_SPACE_WORDS)
+        for word in words:
+            letters = word.split(self.WHITE_SPACE_LETTERS)
+            decoded_letters = [morse_to_char_dict.get(letter, "") for letter in letters]
+            decoded_words.append("".join(decoded_letters))
 
-    decoded_words = []
-
-    for word in words:
-        letters = word.split(WHITE_SPACE_LETTERS)
-        decoded_letters = [morse_to_char_dict.get(letter, "") for letter in letters]
-        decoded_words.append("".join(decoded_letters))
-
-    return " ".join(decoded_words)
+        return " ".join(decoded_words)
 
 
 # def text_to_morse(text: str):
@@ -86,10 +95,10 @@ def morse_to_text(morse: str):
 
 #     return "   ".join(morse_words)
 
-
-hola_mundo_to_morse = text_to_morse("hola mundo")
+converter = MorseConverter()
+hola_mundo_to_morse = converter.text_to_morse("hola mundo")
 print(f"hola_mundo en morse: {hola_mundo_to_morse}")
 
 
-decoded_morse = morse_to_text(hola_mundo_to_morse)
+decoded_morse = converter.morse_to_text(hola_mundo_to_morse)
 print(f"Decoded morse: {decoded_morse}")
